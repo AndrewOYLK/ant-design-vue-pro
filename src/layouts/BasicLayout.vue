4d0cd6da0eafe9237464dac25eeba1e92ca42f0f
andrew <13631263246@163.com>
2021-09-21 23:51:27 +0800

    basic code

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <a-layout id="components-layout-demo-side" style="min-height: 100vh">
-      <a-layout-sider v-model="collapsed" collapsible>
+      <a-layout-sider :trigger="null" v-model="collapsed" collapsible>
         <div class="logo" />
         <SiderMenu></SiderMenu>
       </a-layout-sider>
       <a-layout>
         <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          ></a-icon>
           <Header></Header>
         </a-layout-header>
         <a-layout-content style="margin: 0 16px">
@@ -39,10 +44,13 @@ export default {
 };
 </script>
 
-<style>
-#components-layout-demo-side .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
+<style scoped>
+.trigger {
+  padding: 0 20px;
+  line-height: 64px;
+}
+
+.trigger:hover {
+  background-color: #eee;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import store from "./store";
 // import { Button } from "ant-design-vue/lib/button";
 // import "ant-design-vue/dist/antd.less";
 // import "ant-design-vue/lib/button/style";
-import { Button, Layout } from "ant-design-vue";
+import { Button, Layout, Icon } from "ant-design-vue";
 
 Vue.config.productionTip = false;
 
 // Vue.use(Antd);
 Vue.use(Button);
 Vue.use(Layout);
+Vue.use(Icon);
 
 new Vue({
   router,
